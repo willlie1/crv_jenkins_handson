befdb83fcf4a346e085992d368ce44c6955350f8
Bart van der Hoek <bart.van.der.hoek@crv4all.com>
2020-06-08 14:42:54 +0200

    fixed wrongly added spaces in the compare string

--- src/test/java/com/crv/jenkinshandson/HelloControllerTest.java
+++ src/test/java/com/crv/jenkinshandson/HelloControllerTest.java
@@ -1,17 +1,16 @@
 package com.crv.jenkinshandson;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,6 +23,6 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Sprin          g Boot!")));
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
 	}
 }
